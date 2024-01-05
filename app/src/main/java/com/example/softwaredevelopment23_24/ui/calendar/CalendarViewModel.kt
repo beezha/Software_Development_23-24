0af01560aafdc85288004936fc8d37a946a4d1fb
Misha Ilagan <mishalarice@gmail.com>
2024-01-05 01:54:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/softwaredevelopment23_24/ui/calendar/CalendarViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.softwaredevelopment23_24.ui.calendar
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalendarViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Software Development 23-24"
+include(":app")
