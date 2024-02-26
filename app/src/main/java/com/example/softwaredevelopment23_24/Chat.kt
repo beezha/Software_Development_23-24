620af8fc014294943f4b958cb3151bb78e34d70c
Misha Ilagan <mishalarice@gmail.com>
2024-02-25 23:51:11 -0500

    added calendar adapter

--- app/src/main/java/com/example/softwaredevelopment23_24/Chat.kt
+++ app/src/main/java/com/example/softwaredevelopment23_24/Chat.kt
@@ -34,7 +34,7 @@ class Chat : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        return inflater.inflate(R.layout.fragment_beginning_chat, container, false)
     }
 
     companion object {
