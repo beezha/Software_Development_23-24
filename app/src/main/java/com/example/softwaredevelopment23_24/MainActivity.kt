af2e2d79487dd504caefb6605e34ff77bf43a39c
cnnr137 <connerlennon137@gmail.com>
2024-03-24 18:41:51 -0400

    Added features == changed where the settings menu item would lead to depending if the user is signed in or not. not being signed in would lead to login fragment and being logged in would lead to settings fragment == linked app with a Firebase project
    Added files
    == Login.kt (login fragment)
    == fragment_login.xml (login fragment design)
    == google-services.json (needed for google authentication
    
    Edits made
    == changed layout of chat fragment for future development
    == changed logic behind the inflation of fragments in ActivityMain.kt for
    the login feature
    
    TODO
    == add email and password authentication
    == add google authentication

--- app/src/main/java/com/example/softwaredevelopment23_24/MainActivity.kt
+++ app/src/main/java/com/example/softwaredevelopment23_24/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.softwaredevelopment23_24
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.softwaredevelopment23_24.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,13 +22,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_chat, R.id.navigation_calendar))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        // Determine if the user is authenticated and make a boolean value
+        val isAuthenticated = FirebaseAuth.getInstance().currentUser != null
+
+        /* This overly long and complicated code is parsing each view id and
+            pairing it with the id of the fragments. This is needed
+            so the the settings menu item will either go to the login
+            fragment or the settings fragment depending on user
+            authentication status.
+         */
+        navView.selectedItemId = R.id.navigation_home
+
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.navigation_home)
+                    true
+                }
+
+                R.id.navigation_chat -> {
+                    navController.navigate(R.id.navigation_chat)
+                    true
+                }
+
+                R.id.navigation_calendar -> {
+                    navController.navigate(R.id.navigation_calendar)
+                    true
+                }
+
+                R.id.navigation_pet -> {
+                    navController.navigate(R.id.navigation_pet)
+                    true
+                }
+
+                R.id.navigation_settings -> {
+                    if (isAuthenticated) {
+                        navController.navigate(R.id.navigation_settings_authenticated)
+                    } else {
+                        navController.navigate(R.id.navigation_settings_unauthenticated)
+                    }
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
