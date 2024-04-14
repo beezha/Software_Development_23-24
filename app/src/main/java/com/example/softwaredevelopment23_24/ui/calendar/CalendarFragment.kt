7e70295f19d7485b540f22dddf63e9025308fd47
Misha Ilagan <mishalarice@gmail.com>
2024-04-14 14:03:56 -0400

    finished fragment_calendar.xml layout and CalendarFragment.kt code

--- app/src/main/java/com/example/softwaredevelopment23_24/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/softwaredevelopment23_24/ui/calendar/CalendarFragment.kt
@@ -1,11 +1,26 @@
 package com.example.softwaredevelopment23_24.ui.calendar
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.appbar.AppBarLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.widget.NestedScrollView
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import androidx.fragment.app.Fragment
 import com.example.softwaredevelopment23_24.databinding.FragmentCalendarBinding
+import com.example.softwaredevelopment23_24.R
+import android.widget.GridView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.softwaredevelopment23_24.task_Confirm
+import java.util.*
+import kotlin.math.abs
 
 class CalendarFragment : Fragment() {
 //
@@ -43,57 +58,33 @@ class CalendarFragment : Fragment() {
 //    }
 
     private var _binding: FragmentCalendarBinding? = null
-    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
-//        val view = binding.root
-//
-//        // Set up your collapsing toolbar here
-//        binding.calendarToolbar.title = "CALENDAR"
-//
-//        return view
-//    }
+    private val binding get() = _binding?: error("FragmentCalendarBinding not initialized")
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private val tasks = listOf(
-        listOf("Drink 5 Cups of Water",0,15),
-        listOf("Spend 30 Minutes Time Outside",1,15),
-        listOf("Exercise For 20 Minutes",1,15),
-        listOf("Maintain Good Hygiene",0,15),
-        listOf("Meditate For 10 Minutes",1,15),
-        listOf("Task Spot Filler 1",1,15),
-        listOf("Task Spot Filler 2",1,15),
-        listOf("Task Spot Filler 3",1,15),
-        listOf("Completed Task",1,15)
-    )
-    private var avaTasks = mutableListOf(0,1,2,3,4,5,6,7,8,8,8,8,8,8,8,8)
-
-
-    fun removeTask(taskIndex: Int) {
-        avaTasks.removeAt(taskIndex)
-    }
-    private fun showTaskCon(taskIndex: Int) {
-
-        task_Confirm(this,taskIndex).show(childFragmentManager, "task_Confirm.kt")
-
-    }
-    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+
         val view = binding.root
+
+        val collapsingToolbar = view.findViewById<CollapsingToolbarLayout>(R.id.collapsingToolbar)
+        val toolbarText = view.findViewById<TextView>(R.id.toolbarText)
+        val startSize = resources.getDimensionPixelSize(R.dimen.start_size)
+        val endSize = resources.getDimensionPixelSize(R.dimen.end_size)
+        val textSizeAnimator = ValueAnimator.ofInt(startSize, endSize).apply {
+            addUpdateListener { valueAnimator ->
+                val animatedValue = valueAnimator.animatedValue as Int
+                toolbarText.setTextSize(TypedValue.COMPLEX_UNIT_PX, animatedValue.toFloat())
+            }
+        }
+
+        val appBarLayout = view.findViewById<AppBarLayout>(R.id.appBarLayout)
+        appBarLayout.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val percentage = abs(verticalOffset).toFloat() / appBarLayout.totalScrollRange
+            textSizeAnimator.currentPlayTime = (percentage * textSizeAnimator.duration).toLong()
+        })
 
         val calendarGridView = view.findViewById<GridView>(R.id.calendarGridView)
         val days = getDaysOfMonth()
@@ -128,6 +119,34 @@ class CalendarFragment : Fragment() {
 
         refreshTasks()
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private val tasks = listOf(
+        listOf("Drink 5 Cups of Water",0,15),
+        listOf("Spend 30 Minutes Time Outside",1,15),
+        listOf("Exercise For 20 Minutes",1,15),
+        listOf("Maintain Good Hygiene",0,15),
+        listOf("Meditate For 10 Minutes",1,15),
+        listOf("Task Spot Filler 1",1,15),
+        listOf("Task Spot Filler 2",1,15),
+        listOf("Task Spot Filler 3",1,15),
+        listOf("Completed Task",1,15)
+    )
+    private var avaTasks = mutableListOf(0,1,2,3,4,5,6,7,8,8,8,8,8,8,8,8)
+
+
+    fun removeTask(taskIndex: Int) {
+        avaTasks.removeAt(taskIndex)
+    }
+    private fun showTaskCon(taskIndex: Int) {
+
+        task_Confirm(this,taskIndex).show(childFragmentManager, "task_Confirm.kt")
+
     }
 
     private fun getDaysOfMonth(): List<Date> {
